28f0d77595e4966097c492a20d31c02a0f796889
Grokwik <frederic@bodin.site>
2017-01-04 11:19:57 +0100

    Initial push

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using NFluent;
+
+namespace Project
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Solution.DoIt();
+        }
+    }
+}
